99609f62fc181a61f1c0deacdf37de385f8ab130
Jorge Astorga <jorge.astorga@jorgeasto-ltm2.internal.salesforce.com>
2016-09-07 15:56:35 -0700

    fixed a bug in the printing function inside the handler

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
-  fmt.Fprint(w, "Hi there, %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
 }
 
 func main(){
+
+  //Routing: mapping the handler function to a request path
+  //This is the base funcitonality for web frameworks. Map a request to
+  //a function.
   http.HandleFunc("/", handler)
+
+
   port := os.Getenv("PORT")
 
   if port == "" {
