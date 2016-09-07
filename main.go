ea87a32eab4f935f35fbb2f9f4f16d166db0d2a5
Jorge Astorga <jorge.astorga@jorgeasto-ltm2.internal.salesforce.com>
2016-09-06 17:20:55 -0700

    added configurable port

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
   "fmt"
   "net/http"
+  "os"
+  "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
@@ -11,5 +13,12 @@ func handler(w http.ResponseWriter, r *http.Request){
 
 func main(){
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    log.Fatal("$PORT must be set")
+    }
+
+  //http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":" + port, nil)
 }
