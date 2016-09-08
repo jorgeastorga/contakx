313916881fbc9e3c8f72a135adb7e1a3e4b1504f
Jorge E. Astorga <jeastorga@gmail.com>
2016-09-07 20:56:14 -0700

    added files and changed main code to wiki.go

--- wiki.go
+++ wiki.go
@@ -1,8 +1,11 @@
-/*package main
+package main
 
 import (
   "fmt"
   "io/ioutil"
+  "net/http"
+  "os"
+  //"log"
 )
 
 
@@ -25,10 +28,29 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request){
+  title := r.URL.Path[len("/view/"):]
+  p,_ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
+}
+
 func main() {
   p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
   p1.save()
   p2,_ := loadPage("TestPage")
   fmt.Println(string(p2.Body))
+
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    log.Fatal("$PORT must be set")
+    }
+
+    http.HandleFunc("/view/", viewHandler)
+    http.ListenAndServe(":" + port, nil)
+    //http.ListenAndServe(":8080", nil)
 }
-*/
